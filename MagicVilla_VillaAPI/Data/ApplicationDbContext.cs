b9ef2a07858e903f07e7971a47140225ae2cd2bf
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-25 00:05:33 -0300

    seeding villa table

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -10,5 +10,23 @@ namespace MagicVilla_VillaAPI.Data
             
         }
         public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Details = "Fusce 11 tincidunt max...",
+                    ImageUrl = "",
+                    Occupancy = 5,
+                    Rate = 200,
+                    Sqft = 550,
+                    Amenity = "",
+                    CreatedDate = DateTime.Now
+                });
+        }
+
     }
 }
